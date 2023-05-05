6d7cf938152b5692dcbf5ba14fa89c23b1a8b148
VeselinaTsatseva <56478246+VeselinaTsatseva@users.noreply.github.com>
2023-05-06 00:05:03 +0300

    Add files via upload

---
+++ CreateTables.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE Bookstore;
+
+CREATE TABLE Books (
+	isbn INT PRIMARY KEY NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    author VARCHAR(50) NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(5, 2) NOT NULL
+);
+
+CREATE TABLE Users (
+	userID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    pwd VARCHAR(50) NOT NULL,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(15) NOT NULL
+);
+
+ALTER TABLE Users
+  ADD CONSTRAINT UNIQUE (username),
+  ADD CONSTRAINT UNIQUE (email);
+
+CREATE TABLE Orders (
+	orderID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    date_created Date NOT NULL,
+    total DECIMAL(5, 2) NOT NULL,
+    userID INT NOT NULL,
+    FOREIGN KEY(userID) REFERENCES Users(userID)
+);
+
+CREATE TABLE OrderedItem (
+	orderedItemID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    orderID INT NOT NULL,
+    isbn INT NOT NULL,
+    price DECIMAL(5, 2) NOT NULL,
+    FOREIGN KEY(isbn) REFERENCES Books(isbn),
+    FOREIGN KEY(orderID) REFERENCES Orders(orderID)
+);
+
+ALTER TABLE OrderedItem ADD COLUMN isRented VARCHAR(10);
